d882913f67434fc7676d3c7b973a18a39392c4c2
茅逸文 <maoyiwen@maoyiwendeMacBook-Air.local>
2016-10-21 16:46:32 +0200

    forecast

---
+++ config/initializers/forecast.rb
@@ -0,0 +1,3 @@
+ForecastIO.configure do |configuration|
+    configuration.api_key = '581a3dde696989c8bf628a35377698b2'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :cities
-
-  resources :students
+    resources :cities
+    
+    resources :students
+    
+    resources :forecasts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
