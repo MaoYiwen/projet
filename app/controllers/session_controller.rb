59762876d15ab8d48015a782ee5b645693295246
maoyiwen0212 <maoyiwen0212@gmail.com>
2016-11-29 09:34:18 -0500

    ''(''

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,8 +2,7 @@ class SessionController < ApplicationController
    def create
     token = request.env['omniauth.auth'].credentials.token
    
-      RestClient.get('https://api.github.com/gists/aa5a315d61ae9438b18d',              
-                                      (:params => {:access_token => token}))
+      RestClient.get('https://api.github.com/gists/aa5a315d61ae9438b18d',(:params => {:access_token => token}))
     
     render text: request.env['omniauth.auth'].to_yaml
   end
